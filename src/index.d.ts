3bcc8c1c4ad1058b9244fa021cb856a4db90ebf4
The_Pr0fessor <trendonrobinson@gmail.com>
2025-11-09 18:28:54 -0500

    Update version to 1.0.4 and remove GuiControllerSettings interface from index.d.ts

--- src/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface GuiControllerSettings {
-	HideWithState?: boolean;
-	CanHide?: boolean;
-	Visible?: boolean;
-	DisabledCoreGuis?: Enum.CoreGuiType[];
-	PopUp?: boolean;
-	IgnoreGuiInset?: boolean;
-	DisplayOrder?: number;
-}
